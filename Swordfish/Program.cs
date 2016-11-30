ddd38e8c66b9917608ba1cf32244302dde44e14b
vmdharan <rexar7@gmail.com>
2016-11-30 13:47:30 +1100

    Initial commit
    - Include required SharpDX dependencies.
    - Test the initial build by echo-ing "Hello World!".

---
+++ Swordfish/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swordfish
+{
+    class Program
+    {
+        private static void Main()
+        {
+            Console.Write("Hello World!");
+            while(true) { }
+        }
+    }
+}
